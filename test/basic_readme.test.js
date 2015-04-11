468d3456f122aca1e791ee31c4aa922679ffe9cf
joshWyatt <joshuawyatt123@gmail.com>
2015-04-11 09:37:49 -0700

    Create blank README when run

--- test/basic_readme.test.js
+++ test/basic_readme.test.js
@@ -2,16 +2,35 @@ var test = require('tape');
 var async = require('async');
 var fileExists = require('file-exists');
 var basicReadme = require('../lib/basic_readme.js');
+var fs = require('fs');
 
-test('lib/basic_readme.js', function(t){
-  t.plan(1);
+async.series([runBasicReadme, testBasicReadme], cleanUp);
+
+function runBasicReadme(cb) {
 
   var options = {
     moduleName: 'test_module',
-    description: 'This is just a test module for running tests with';
+    description: 'This is just a test module for running tests with',
+    filePath: __dirname
   };
 
   basicReadme(options);
-  t.pass('it should start without errors');
-  
-});
+  cb();
+}
+
+function testBasicReadme(cb) {
+  test('lib/basic_readme.js', function(t){
+    t.plan(2);
+
+    t.pass('it should start without errors');
+    t.ok(fileExists(__dirname + '/README.md'), 'it should create a README.md');
+    
+    cb();
+  });
+}
+
+function cleanUp(err) {
+  if (err) throw err;
+
+  fs.unlinkSync(__dirname + '/README.md');
+}
