7a7e6a326c58e97f3336b70ae5f338a5d855930c
joshWyatt <joshuawyatt123@gmail.com>
2015-04-11 08:17:24 -0700

    Add test for index.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,9 @@
 var test = require('tape');
 var fileExists = require('file-exists');
+var basicReadme = require('../');
 
 test('index.js', function(t){
   t.plan(1);
 
-  t.pass('it should pass');
+  t.pass('it should run without an error');
 });
