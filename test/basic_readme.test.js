368b5ca6d0800563ae10b4ec791f81f6a75db2ca
joshWyatt <joshuawyatt123@gmail.com>
2015-04-11 08:41:25 -0700

    Change initial basic_readme.test

--- test/basic_readme.test.js
+++ test/basic_readme.test.js
@@ -1,7 +1,17 @@
 var test = require('tape');
+var async = require('async');
+var fileExists = require('file-exists');
+var basicReadme = require('../lib/basic_readme.js');
 
 test('lib/basic_readme.js', function(t){
   t.plan(1);
 
-  t.pass('t.pass');
+  var options = {
+    moduleName: 'test_module',
+    description: 'This is just a test module for running tests with';
+  };
+
+  basicReadme(options);
+  t.pass('it should start without errors');
+  
 });
