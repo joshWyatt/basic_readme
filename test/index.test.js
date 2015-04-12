a162675d867d1e2704f81069e0cbe945e05d9a07
joshWyatt <joshuawyatt123@gmail.com>
2015-04-12 11:19:37 -0700

    Write index.js test

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,10 @@ var fileExists = require('file-exists');
 
 test('index.js', function(t){
   t.plan(1);
-
   t.pass('it should run without an error');
+
+  // mocking basicReadme for test
+  var basicReadme = function() {
+    t.equal(arguments.length, 2, 'it should call basicReadme with 2 arguments');
+  };
 });
