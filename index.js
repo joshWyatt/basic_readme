b21c0b1496591ee83f7dcd09465390c659d5b226
joshWyatt <joshuawyatt123@gmail.com>
2015-04-12 12:13:57 -0700

    Update to use new version 1.0.4 print_usage module

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ var argv = require('minimist')(process.argv.slice(2), {
 
 var argvHasProblems = require('./lib/argv_has_problems.js');
 var printUsageAndExit = require('print_usage');
+var pathToUsage = __dirname + '/usage.md';
 var parseHeadersFromArgv = require('./lib/parse_headers_from_argv.js');
 var repoName;
 var headers;
 
 // see usage.md for Usage prompt
-if (argvHasProblems(argv)) printUsageAndExit();
+if (argvHasProblems(argv)) printUsageAndExit(pathToUsage);
 
 repoName = argv._;
 headers = parseHeadersFromArgv(argv);
